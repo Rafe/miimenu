878349dee51aa4f8a58fe36a883ffaa12b2c4b2b
Jimmy Chao <daizenga@gmail.com>
2011-09-03 16:16:03 -0400

    [omniauth]complete twitter login sample

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,11 +12,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def twitter
-    @user = User.first
-    #auth_data = env["mmniauth.auth"]["extra"]["user_info"]
-
-    #user = User.find_by_name(auth_data["user_info"]["name"])
-    #render :text => env["omniauth.auth"] ,:format => :yaml
-    sign_in_and_redirect @user, :event => :authentication
+    auth_data = env["mmniauth.auth"]["extra"]
+    @user = User.find_by_name(auth_data["user_info"]["name"])
+    if @user.persisted?
+      flash[:notice] = "login with twitter success"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.facebook_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 end
