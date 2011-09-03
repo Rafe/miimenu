580214c1faabef7b49ce5fd3b989e6260e53bf72
Jimmy Chao <daizenga@gmail.com>
2011-09-03 16:33:35 -0400

    [debug]for twitter, again

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,7 +17,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if user_hash["name"] != "Jimmy Chao"
       render :text => user_hash["name"]
     else
-      @user = User.find_by_name(user_hash["name"])
+      @user = User.find_by_name(user_hash["name"].to_s)
       if @user.persisted?
         flash[:notice] = "login with twitter success"
         sign_in_and_redirect @user, :event => :authentication
