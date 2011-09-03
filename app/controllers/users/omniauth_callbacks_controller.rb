d4b84fd41b7826b937f77d812531c5eeb4efe1a0
Jimmy Chao <daizenga@gmail.com>
2011-09-03 16:55:19 -0400

    [debug]for dumping facebook oauth format

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController 
   def facebook 
     @user = User.find_for_facebook_oauth(env["omniauth.auth"],current_user)
-
+    render :text => env["omniauth.auth"]
     if @user.persisted?
       flash[:notice] = "login with facebook success"
       sign_in_and_redirect @user, :event => :authentication
@@ -13,18 +13,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
     user_hash = env["omniauth.auth"]["extra"]["user_hash"]
-    #render :text => user_hash["name"]
-    if user_hash["name"] != "Jimmy Chao"
-      render :text => user_hash["name"]
+    #use user_name for authentication sample, need to change the authentication to other
+    @user = User.find_by_name(user_hash["name"].to_s)
+    if @user.persisted?
+      flash[:notice] = "login with twitter success"
+      sign_in_and_redirect @user, :event => :authentication
     else
-      @user = User.find_by_name(user_hash["name"].to_s)
-      if @user.persisted?
-        flash[:notice] = "login with twitter success"
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        session["devise.twitter_data"] = env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+      session["devise.twitter_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
 end
